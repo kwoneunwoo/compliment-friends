aa1f4df696fdc6a83e96d33047a5e0659fe4aa37
chord <hadani1023@gmail.com>
2023-11-02 15:31:20 +0900

    modified to use webhook

--- compliment.js
+++ compliment.js
@@ -2,14 +2,19 @@ function AddCompliment() {
   const message = document.getElementById('CommentContainer').value;
   const number = document.getElementById('identifier').value;
 
-  if(message&&number){
-    const text = document.createElement( 'p' );
-    const compliment = document.createTextNode(number+" || "+message);
-    text.appendChild(compliment);
-    document.body.appendChild(text);
+  const http = new XMLHttpRequest();
+  const url = 'https://discord.com/api/webhooks/1169484351650680943/m13a6wTgW4VK4PM1oEXxQqK33YOqwrZMdG1jEzKBD_6rHUmbLsW5lYSTfnloQQHmRMfG';
 
-    document.getElementById('CommentContainer').value = '';
+  if(message&&number){
+    http.open("POST", url);
+    http.setRequestHeader('Content-type', 'application/json');
+    const param = {
+      content : number+" || "+message
+    }
+  http.send(JSON.stringify(param));
+
+  document.getElementById('CommentContainer').value = '';
   } else {
     alert('학번과 칭찬을 모두 입력해주세요');
   }
-} 
+}
